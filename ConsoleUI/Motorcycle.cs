56f2b9885bc48a05daba61de74ecbd79e8687efd
Bailey Hart <baileyhart100@gmail.com>
2023-06-07 14:58:51 -0400

    initial commit

---
+++ ConsoleUI/Motorcycle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    public class Motorcycle :Vehicle
+    {
+        public  Motorcycle()
+        {
+
+        }
+
+        public bool HasSideCart { get; set; } = false;
+
+        public override void DriveAbstract()
+        {
+            Console.WriteLine($"This motorcycle is in drive!");
+        }
+
+        public override void DriveVirtual()
+        {
+            Console.WriteLine($"This {GetType().Name} is a {GetType().BaseType} that is virtually in drive");
+        }
+    }
+}
